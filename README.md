6e64f51eed1781221a86e6591243d376ad847964
niderman <viktor.niderman@gmail.com>
2024-02-24 11:09:07 +0200

    change styles

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # regExp
 
-[Site](https://zeref95.github.io/regExp/)
+A website designed to facilitate the understanding of how regular expressions work.
+
+[Heel free to have a look](https://viktor-niderman.github.io/regExp/)
--- script.js
+++ script.js
@@ -1,110 +1,121 @@
 const { createApp } = Vue
 
-        createApp({
-            methods: {
-                setCurrentFlagInfo(str) {
-                    this.selectedFlagInfo = str;
-                },
-                enableMod(index) {
-                    this.modes.forEach((v, i) => {
-                        if (i !== index) {
-                            this.modes[i].enable = false;
-                        } else {
-                            this.modes[i].enable = !this.modes[i].enable;
-                        }
-                    })
-                }
-            },
-            computed: {
-                result() {
-                    this.expression.isCorrect = true;
-                    if (!this.expression.string) return '⌛';
+createApp({
+  methods: {
+    setCurrentFlagInfo (str) {
+      this.selectedFlagInfo = str
+    },
+    enableMod (index) {
+      this.modes.forEach((v, i) => {
+        if (i !== index) {
+          this.modes[i].enable = false
+        } else {
+          this.modes[i].enable = !this.modes[i].enable
+        }
+      })
+    },
+  },
+  computed: {
+    result () {
+      this.expression.isCorrect = true
+      if (!this.expression.string) return ''
 
-                    try {
-                        let reg = new RegExp(this.expression.string, this.generatedFlags);
-                        let rez = '';
-                        if (this.modes[0].enable) {
-                            rez = this.text.match(reg);
-                        } else if (this.modes[1].enable) {
-                            rez = reg.test(this.text);
-                        } else if (this.modes[2].enable) {
-                            this.replacedText = this.text.replace(reg, this.replaceInput);
-                            rez = "⬇";
-                        }
-                        return rez === null ? 'null' : rez;
-                    } catch (e) {
-                        this.expression.isCorrect = false;
-                        return '⛔';
-                    }
-                    
-                },
-                generatedFlags() {
-                    let str = '';
-                    this.flags.forEach(flag => {
-                        if (flag.enable) {str += flag.name}
-                    })
-                    return str;
-                },
-            },
-            data() {
-                return {
-                    modes: [
-                        {name: 'match', enable: true},
-                        {name: 'test', enable: false},
-                        {name: 'replace', enable: false},
-                    ],
-                    flags: [
-                        {name: 'g', enable: true, info: 'Поиск ищет все совпадения, без него – только первое'},
-                        {name: 'i', enable: false, info: 'Регистронезависимость'},
-                        {name: 'm', enable: false, info: 'Многострочный режим'},
-                        {name: 's', enable: false, info: 'Включает режим «dotall», при котором точка . может соответствовать символу перевода строки \\n'},
-                        {name: 'u', enable: false, info: 'Включает полную поддержку Юникода'},
-                        {name: 'y', enable: false, info: 'Режим поиска на конкретной позиции в тексте'},
-                    ],
-                    selectedFlagInfo: '',
-                    expression: {
-                        string: '',
-                        isCorrect: true,
-                    },
-                    originText: '',
-                    text: '',
-                    replacedText: '',
-                    replaceInput: '',
-                    needHelp: false,
-                }
-            },
-            mounted() {
-                window.onload = () => {
-                    this.originText = window.text;
-                    this.text = this.originText;
-                }
-                
+      try {
+        let reg = new RegExp(this.expression.string, this.generatedFlags)
+        let rez = ''
+        if (this.modes[0].enable) {
+          rez = this.text.match(reg)
+        } else if (this.modes[1].enable) {
+          rez = reg.test(this.text)
+        } else if (this.modes[2].enable) {
+          this.replacedText = this.text.replace(reg, this.replaceInput)
+          rez = '⬇'
+        }
+        return rez === null ? 'null' : rez
+      } catch (e) {
+        this.expression.isCorrect = false
+        return '⛔ Syntax error in RegExp'
+      }
 
-                document.addEventListener('mouseup', event => {  
-                    let selection = window.getSelection();
-                    let ext = selection.extentNode;
-                    let isMainText = false;
-                    for (let i = 0; i < 5; i++) {
-                        if (ext?.id === 'mainText') {
-                            isMainText = true;
-                            break;
-                        }
-                        try {
-                            ext = ext.parentNode;
-                        } catch (e) {
-                            break;
-                        }
-                    }
+    },
+    generatedFlags () {
+      let str = ''
+      this.flags.forEach(flag => {
+        if (flag.enable) {str += flag.name}
+      })
+      return str
+    },
+  },
+  data () {
+    return {
+      modes: [
+        { name: 'match', enable: true },
+        { name: 'test', enable: false },
+        { name: 'replace', enable: false },
+      ],
+      flags: [
+        {
+          name: 'g',
+          enable: true,
+          info: 'Search finds all matches; without it, only the first match is found',
+        },
+        { name: 'i', enable: false, info: 'Case-insensitivity' },
+        { name: 'm', enable: false, info: 'Multiline mode' },
+        {
+          name: 's',
+          enable: false,
+          info: 'Enables "dotall" mode, where the dot . can match a newline character \n',
+        },
+        { name: 'u', enable: false, info: 'Enables full Unicode support' },
+        {
+          name: 'y',
+          enable: false,
+          info: 'Search mode at a specific position in the text',
+        },
+      ],
+      selectedFlagInfo: '',
+      expression: {
+        string: '',
+        isCorrect: true,
+      },
+      originText: '',
+      text: '',
+      replacedText: '',
+      replaceInput: '',
+      needHelp: false,
+    }
+  },
+  mounted () {
+    window.onload = () => {
+      this.originText = window.text
+      this.text = this.originText
+    }
 
-                    if (isMainText) {
-                        let exactText = selection.toString();
-                        if (exactText) {
-                            this.text = exactText;
-                            return;
-                        }
-                    }
-                    
-                    this.text = this.originText;
-                });
-            },
-        }).mount('#app')
+    document.addEventListener('mouseup', event => {
+      let selection = window.getSelection()
+      let ext = selection.extentNode
+      let isMainText = false
+      for (let i = 0; i < 5; i++) {
+        if (ext?.id === 'mainText') {
+          isMainText = true
+          break
+        }
+        try {
+          ext = ext.parentNode
+        } catch (e) {
+          break
+        }
+      }
+
+      if (isMainText) {
+        let exactText = selection.toString()
+        if (exactText) {
+          this.text = exactText
+          return
+        }
+      }
+
+      this.text = this.originText
+    })
+  },
+}).mount('#app')
